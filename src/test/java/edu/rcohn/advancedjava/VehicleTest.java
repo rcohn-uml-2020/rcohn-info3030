2cf3373c08ebbfb723112a5b3c6c98b39c5147a2
rcohn-uml-2020 <39661635+rcohn-uml-2020@users.noreply.github.com>
2018-06-03 01:51:55 -0400

    update

--- src/test/java/edu/rcohn/advancedjava/VehicleTest.java
+++ src/test/java/edu/rcohn/advancedjava/VehicleTest.java
@@ -1,51 +1,65 @@
 package edu.rcohn.advancedjava;
 
-import org.junit.Before;
-import org.junit.Test;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import java.util.*;
 
-public class VehicleTest {
+public class VehicleTest extends TestCase {
 
-    private List<string> wheels;
+    /**
+     * Create the test case
+     *
+     * @param vehicleTest name of the test case
+     */
+    public VehicleTest( String vehicleTest )
+    {
+        super( vehicleTest );
+    }
+
+    private List<String> wheels;
     private String color;
-    private Vehicle vehicle;
+    Vehicle vehicle;
 
-    @Before
-    public void setup() {
-        wheels = new List<string>;
+    public void setUp() throws Exception {
+        super.setUp();
+        wheels = new ArrayList<String>();
+        color = "";
+        vehicle = new Vehicle(wheels, color);
+        vehicle.setColor("Blue");
         wheels.add("Front Left");
         wheels.add("Front Right");
         wheels.add("Back Left");
-        this.color = "Blue";
-        vehicle = new Vehicle(List<string> wheels, color);
+        vehicle.setWheels(wheels);
     }
 
-    @Test
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( VehicleTest.class );
+    }
+
     public void testGetColorPositive() {
-        assertTrue("Blue", vehicle.getColor());
+        assertEquals("Blue", vehicle.getColor());
     }
 
     // NOTICE: it is a good idea to write tests that verify expected behavior in both positive and negative ways.
     // ALSO NOTICE: I have separate tests for each specific test. Don't bunch up a tests into one test method.
-    @Test
     public void testGetColorNegative() {
-        vehicle.setColor("Red");
-        assertFalse("Blue", vehicle.getColor());
+        assertNotSame("Red", vehicle.getColor());
     }
 
-    @Test
     public void testGetWheelsNumberPositive() {
-        assertTrue(3, vehicle.getWheelsNumber());
+        assertEquals(3, vehicle.getWheelsNumber());
     }
 
     // NOTICE: it is a good idea to write tests that verify expected behavior in both positive and negative ways.
     // ALSO NOTICE: I have separate tests for each specific test. Don't bunch up a tests into one test method.
-    @Test
-    public void testGetWheelsNumberNegative() {
-        assertFalse(4, vehicle.getWheelsNumber());
-    }
 
+    public void testGetWheelsNumberNegative() {
+        assertNotSame( 4, vehicle.getWheelsNumber());
+    }
 }
