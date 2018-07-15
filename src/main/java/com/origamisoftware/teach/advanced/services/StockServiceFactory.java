515ff17902cdd076fe5a38683cc85d73155a1796
rcohn-uml-2020 <39661635+rcohn-uml-2020@users.noreply.github.com>
2018-07-15 19:51:24 -0400

    unneeded imports for StockServiceFactory

--- src/main/java/com/origamisoftware/teach/advanced/services/StockServiceFactory.java
+++ src/main/java/com/origamisoftware/teach/advanced/services/StockServiceFactory.java
@@ -1,13 +1,5 @@
 package com.origamisoftware.teach.advanced.services;
 
-import com.origamisoftware.teach.advanced.model.StockQuote;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-
 /**
  * A factory that returns a <CODE>StockService</CODE> instance.
  */
