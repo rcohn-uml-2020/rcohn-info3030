20b2295984f01b978e9ca6af5a6167c83e851dd5
rcohn-uml-2020 <39661635+rcohn-uml-2020@users.noreply.github.com>
2018-07-18 10:22:10 -0400

    Update stocks_db_initialization.sql

--- src/main/sql/stocks_db_initialization.sql
+++ src/main/sql/stocks_db_initialization.sql
@@ -42,3 +42,15 @@ INSERT INTO quote (symbol,time,price) VALUES ('GOOG','2004-08-19 00:00:01','85.0
 INSERT INTO quote (symbol,time,price) VALUES ('GOOG','2015-02-03 00:00:01','527.35');
 INSERT INTO quote (symbol,time,price) VALUES ('APPL','2000-01-01 00:00:01','118.27');
 INSERT INTO quote (symbol,time,price) VALUES ('AMZN','2015-02-03 00:00:01','363.21');
+
+INSERT INTO person (user_name) VALUES ('Tom');
+INSERT INTO person (user_name) VALUES ('Dick');
+INSERT INTO person (user_name) VALUES ('Harry');
+
+INSERT INTO person_stocks ( person_id ) VALUES (1);
+INSERT INTO person_stocks ( person_id ) VALUES (2);
+INSERT INTO person_stocks ( person_id ) VALUES (3);
+
+INSERT INTO stock_symbol (symbol) VALUES ('GOOG');
+INSERT INTO stock_symbol (symbol) VALUES ('APPL');
+INSERT INTO stock_symbol (symbol) VALUES ('AMZN');
