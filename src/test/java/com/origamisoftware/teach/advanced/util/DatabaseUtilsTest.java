f75df78cc097a18cfa120dce11f1b49708e7e0cf
rcohn-uml-2020 <39661635+rcohn-uml-2020@users.noreply.github.com>
2018-07-20 13:09:59 -0400

    trying to initalize database

--- src/test/java/com/origamisoftware/teach/advanced/util/DatabaseUtilsTest.java
+++ src/test/java/com/origamisoftware/teach/advanced/util/DatabaseUtilsTest.java
@@ -41,14 +41,14 @@ public class DatabaseUtilsTest {
     public void testGetConnectionWorks() throws Exception{
         Connection connection = DatabaseUtils.getConnection();
         Statement statement = connection.createStatement();
-        boolean execute = statement.execute("select * from quote");
-        assertTrue("verify that we can execute a statement against quote table",execute);
-        execute = statement.execute("select * from person");
+        boolean execute = statement.execute("select * from person");
         assertTrue("verify that we can execute a statement against person table",execute);
         execute = statement.execute("select * from stock_symbol");
         assertTrue("verify that we can execute a statement against stock_symbol table",execute);
         execute = statement.execute("select * from person_stocks");
         assertTrue("verify that we can execute a statement against person_stocks table",execute);
+        execute = statement.execute("select * from quote");
+        assertTrue("verify that we can execute a statement against quote table",execute);
 
     }
 
